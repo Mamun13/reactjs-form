0c929fedaa04da526e00c79c36296b5e8590c8d5
Mamun13 <mamuncse82@gmail.com>
2022-07-03 13:42:49 +0600

    form_react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Form from './Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section id='projects'>
+        <div className="container">
+          <h1 className='text-center mt-3 mb-3'>Form</h1>
+          <Form />
+        </div>
+      </section>
+
+    </>
   );
 }
 
---
+++ src/Form.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+function Form() {
+    
+    const [fname, setFname] = useState('');
+    const [lname, setLname] = useState('');
+    const [email, setEmail] = useState('');
+    const [number, setFnumber] = useState('');
+   
+  return (
+    <>
+        
+        <div className="main-div text-center">
+        <h2>{fname} {lname}</h2>
+        <p>{email}</p>
+        <p>{number}</p>
+            <form action=" ">
+                <div className="col-lg-6 text-center">  
+                <input type="text" className='form-control input' name='fname' placeholder='Enter your fname' onChange={(e)=>setFname(e.target.value)} /><br />
+                
+                <input type="text" className='form-control'  onChange={(e)=>setLname(e.target.value)} name='lname' placeholder='Enter your lname' /><br />
+                <input type="text" className='form-control' name='' placeholder='Enter your Number' onChange={(e)=>setFnumber(e.target.value)} /><br />
+                <textarea name="" className='form-control' placeholder='Leave a comment' onChange={(e)=>setEmail(e.target.value)} id="" cols="73" rows="5"></textarea><br />
+                <button className='btn btn-primary ' type='submit' >Submit</button>
+                </div>
+
+            </form>
+        </div>
+         
+    </>
+  )
+}
+
+export default Form
